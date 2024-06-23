2661406dedf189d3ebee4591df509dda624f9e7f
Matheus Ventura <contato.mateusyt@hotmail.com>
2024-06-23 20:04:41 -0300

    add remove choice and messages

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def total_expenses(expenses):
 def filter_expenses_by_category(expenses, category):
     return filter(lambda expense: expense['category'] == category, expenses)
     
+def remove_expense(expenses, amount, category):
+    for expense in expenses:
+        if expense['amount'] == amount and expense['category'] == category:
+            expenses.remove(expense)
+            print(f'Expense {amount} in {category} removed successfully.')
+            return
+    print(f'[!] Expense {amount} in {category} not found')
 
 def main():
     expenses = []
@@ -20,15 +27,22 @@ def main():
         print('2. List all expenses')
         print('3. Show total expenses')
         print('4. Filter expenses by category')
-        print('5. Exit')
+        print('5. Remove category')
+        print('6. Exit')
        
         choice = input('Enter your choice: ')
 
         if choice == '1':
-            amount = float(input('Enter amount: '))
-            category = input('Enter category: ')
-            add_expense(expenses, amount, category)
-
+            try:
+                amount = float(input('Enter amount: '))
+                if amount < 1:
+                    print("[!] Invalid amount")
+                else:
+                    category = input('Enter category: ')
+                    add_expense(expenses, amount, category)
+                    print("Expense added successfully.")
+            except ValueError:
+                print('[!] Invalid amount')
         elif choice == '2':
             print('\nAll Expenses:')
             print_expenses(expenses)
@@ -41,8 +55,17 @@ def main():
             print(f'\nExpenses for {category}:')
             expenses_from_category = filter_expenses_by_category(expenses, category)
             print_expenses(expenses_from_category)
-    
         elif choice == '5':
+            try:
+                amount = float(input('Enter amount to remove: '))
+                category = input(f'Enter category to remove {amount}: ')
+                remove_expense(expenses, amount, category)
+                print(f'Category {category} removed successfully.')
+            except ValueError:
+                print('[!] Invalid amount')
+        elif choice == '6':
             print('Exiting the program.')
             break
+        else:
+            print('Invalid choice. Please enter a number between 1-5.')
 main()
